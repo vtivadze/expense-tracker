f4489e88beac6903055db17768dcf12016dd1085
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-06-29 15:54:58 +0400

    Create ExpenseChart Component

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,12 +2,15 @@ import './Chart.css';
 import ChartBar from './ChartBar';
 
 const Chart = props => {
-  <div className='chart'>
+  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
+
+  return <div className='chart'>
     {props.dataPoints.map(dataPoint => (
       <ChartBar
         key={dataPoint.label}
         value={dataPoint.value}
-        maxValue={null}
+        maxValue={totalMaximum}
         label={dataPoint.label}
       />
     ))}
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = props => {
@@ -21,6 +22,7 @@ const Expenses = props => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
