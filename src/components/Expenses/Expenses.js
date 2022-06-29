a2b291bbef73a2ffba1a5b297b460820a75bba09
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-06-29 11:53:53 +0400

    Fulfill assignment number 3

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,24 +8,29 @@ const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
-    console.log('Old filter: ', filteredYear);
-    console.log('New filter: ', selectedYear);
     setFilteredYear(selectedYear);
   };
 
   return (
     <Card className='expenses'>
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {props.items.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      })}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {props.items
+        .filter(
+          (expense) => expense.date.getFullYear().toString() === filteredYear
+        )
+        .map((expense) => {
+          return (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
     </Card>
   );
 };
